a6d07e7f5466ffe404b7096c87aa2717e814366d
Spencer Henze <spencerhenze@gmail.com>
2017-08-11 10:16:48 -0600

    Back End: Started express server; set up mlab db connection

--- config/db/mlab-config.js
+++ config/db/mlab-config.js
@@ -1,1 +1,17 @@
 //Your MLAB database should be here 
+var mongoose = require('mongoose');
+var connection = mongoose.connection;
+
+
+mongoose.connect('mongodb://spencer:jwufDq5U27Uc@ds127963.mlab.com:27963/inspire_db', {
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+});
+
+connection.on('error', (err) => {
+    console.log('SOMETHING FAILED WHEN CONNECTING TO MLAB', err)
+});
+
+connection.once('open', () => {
+    console.log('SUCCESSFULLY CONNECTED TO MLAB')
+})
--- routes/todos.js
+++ routes/todos.js
@@ -1,1 +1,15 @@
 // You should put your todo schema should go here
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+var todoSchema = new mongoose.Schema({
+    // TODO: finish schema
+});
+
+var Todos = mongoose.model('todo', todoSchema);
+
+//add get, post, put?, and delete functions here:
+
+
+module.exports = router;
