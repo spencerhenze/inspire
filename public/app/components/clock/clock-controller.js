848abf173f2fb5758d4df459bd5efe686f8f5fca
Spencer Henze <spencerhenze@Spencers-iMac.local>
2017-08-11 21:15:56 -0600

    Clock draws but doesn't refresh without exceeding call stack

--- public/app/components/clock/clock-controller.js
+++ public/app/components/clock/clock-controller.js
@@ -2,17 +2,33 @@ function ClockController() {
 
     var clockService = new ClockService();
 
-    function drawClock(time) {
+
+    function checkTime(i) {
+        if (i < 10) { i = '0' + i };
+        return i;
+    }
+
+
+    this.startTime = function () {
+        var today = new Date();
+        var hour = today.getHours();
+        var minute = today.getMinutes();
+        updateTime(today, hour, minute);
+    }
+
+    function updateTime(today, hour, minute) {
+        hour = today.getHours()
+        minute = checkTime(today.getMinutes());
+
+
+        time = `${hour}:${minute}`
 
         template = `<h1>${time}</h1>`
 
+
         document.getElementById('clock').innerHTML = template;
-    }
+        // var t = setTimeout(updateTime(today, hour, minute), 10000)
 
-    this.startTime = function() {
-        debugger
-        clockService.startTime(drawClock);
     }
-
 
 }
--- public/app/components/clock/clock-service.js
+++ public/app/components/clock/clock-service.js
@@ -1,23 +1,22 @@
 function ClockService() {
 
-    function checkTime(i) {
-        if (i < 10) { i = '0' + i };
-        return i;
-    }
+    // function checkTime(i) {
+    //     if (i < 10) { i = '0' + i };
+    //     return i;
+    // }
 
 
-    this.startTime = function (cb) {
-        debugger
-        var today = new Date();
-        var hour = today.getHours();
-        var minute = today.getMinutes();
+    // this.startTime = function (cb) {
+    //     debugger
+    //     var today = new Date();
+    //     var hour = today.getHours();
+    //     var minute = today.getMinutes();
 
-        minute = checkTime(minute);
-        time = `${hour}:${minute}`
-        var t = setTimeout(this.startTime, 500)
+    //     minute = checkTime(minute);
+    //     time = `${hour}:${minute}`
 
-        cb(time);
-    }
+    //     cb(time);
+    // }
 
 
 }
