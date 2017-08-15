ffe1ff5d0f48478518a3e14bd380e49f2218066c
Spencer Henze <spencerhenze@gmail.com>
2017-08-15 16:57:15 -0600

    Updated styling, added toggle button for todo list

--- public/app/components/todo/todo-controller.js
+++ public/app/components/todo/todo-controller.js
@@ -1,49 +1,68 @@
 function TodoController() {
 
 	var todoService = new TodoService()
+	var showTodos = false;
 	var showComplete = false;
+	// var todoCount = 0;
 
+	function getTodoCount(todosArr) {
+		todoCount = 0;
+		todosArr.forEach(todo => {
+			if (todo.complete == false) {
+				todoCount++;
+			}
+		})
+		return todoCount;
+	}
 
 	function drawShowHideButton() {
 		var showHideTemplate = ''
-		if (showComplete === true) {
-			showHideTemplate = `<a href="javascript:void(0)" class="show-complete-button" onclick="app.controllers.todoController.toggleShowHide()"><p>Hide Completed</p></a>`
-		}  // if user chooses to hide completed items: 
-		else if (showComplete === false) {
-			// only append the todo item if the complete property is false
-			showHideTemplate = `<a href="javascript:void(0)" class="show-complete-button" onclick="app.controllers.todoController.toggleShowHide()"><p>Show Completed</p></a>`
+
+		if (showTodos == true) {
+			if (showComplete === true) {
+				showHideTemplate = `<a href="javascript:void(0)" class="show-complete-button" onclick="app.controllers.todoController.toggleShowHide()"><p>Hide Completed</p></a>`
+			}  // if user chooses to hide completed items: 
+			else if (showComplete === false) {
+				// only append the todo item if the complete property is false
+				showHideTemplate = `<a href="javascript:void(0)" class="show-complete-button" onclick="app.controllers.todoController.toggleShowHide()"><p>Show Completed</p></a>`
+			}
+			document.getElementById('show-hide-button').innerHTML = showHideTemplate;
 		}
-		document.getElementById('show-hide-button').innerHTML = showHideTemplate;
+		else {
+			document.getElementById('show-hide-button').innerHTML = '';
+		}
 	}
 
 	function draw(todosArr) {
+
+		var todoCount = getTodoCount(todosArr);
 		drawShowHideButton();
 
-		var todoCount = 0;
+		if (showTodos == true) {
 
-		var template = `
+			var template = `
 			<ul class="no-bullets">
-		`
+			`
 
-		//DONT FORGET TO LOOP
-		var iconIndex = 0
-		var toggleIconId = 'TI' + iconIndex;
+			//DONT FORGET TO LOOP
+			var iconIndex = 0
+			var toggleIconId = 'TI' + iconIndex;
 
-		todosArr.forEach(todo => {
-			var readout = ''
+			todosArr.forEach(todo => {
+				var readout = ''
 
-			//strike out completed item's body properties
-			if (todo.complete == true) {
-				readout = `<del>${todo.body}</del>`;
-			}
-			if (todo.complete == false) {
-				readout = todo.body;
-				todoCount++;
-			}
-			//if user chooses to show completed items, append the item:
-			if (showComplete === true) {
-				showHideTemplate = `<a href="javascript:void(0)" class="show-complete-button" onclick="app.controllers.todoController.toggleShowHide()"><p>Hide Completed</p></a>`
-				template += `
+				//strike out completed item's body properties
+				if (todo.complete == true) {
+					readout = `<del>${todo.body}</del>`;
+				}
+				if (todo.complete == false) {
+					readout = todo.body;
+					// todoCount++;
+				}
+				//if user chooses to show completed items, append the item:
+				if (showComplete === true) {
+					showHideTemplate = `<a href="javascript:void(0)" class="show-complete-button" onclick="app.controllers.todoController.toggleShowHide()"><p>Hide Completed</p></a>`
+					template += `
 				<li class = "todo-item">
 					<a href="javascript:void(0)" data-toggle="tooltip" title="toggleDone" onclick="app.controllers.todoController.toggleTodoStatus('${todo._id}')"><i id="${toggleIconId}"class="fa fa-check-circle-o toggle-icon"></i></a>
 					<p class="todo-text">${readout}</p>
@@ -51,29 +70,40 @@ function TodoController() {
 				</li>
 			`
 
-			}  // if user chooses to hide completed items: 
-			else if (showComplete === false) {
-				// only append the todo item if the complete property is false
-				if (todo.complete == false) {
-					showHideTemplate = `<a href="javascript:void(0)" class="show-complete-button" onclick="app.controllers.todoController.toggleShowHide()"><p>Show Completed</p></a>`
-					template += `
+				}  // if user chooses to hide completed items: 
+				else if (showComplete === false) {
+					// only append the todo item if the complete property is false
+					if (todo.complete == false) {
+						showHideTemplate = `<a href="javascript:void(0)" class="show-complete-button" onclick="app.controllers.todoController.toggleShowHide()"><p>Show Completed</p></a>`
+						template += `
 						<li class = "todo-item">
 							<a href="javascript:void(0)" data-toggle="tooltip" title="toggleDone" onclick="app.controllers.todoController.toggleTodoStatus('${todo._id}')"><i id="${toggleIconId}"class="fa fa-check-circle-o toggle-icon"></i></a>
 							<p class="todo-text">${readout}</p>
 							<a href="javascript:void(0)" onclick="app.controllers.todoController.removeTodo('${todo._id}')"><i class="fa fa-trash delete-icon"></i></a>
 						</li>
 					`
+					}
+					// else { continue; }
 				}
-				// else { continue; }
-			}
-		})
+			})
 
-		template += '</ul>'
+			template += '</ul>'
 
-		document.getElementById('todo').innerHTML = template;
-		iconIndex++;
+			document.getElementById('todo').innerHTML = template;
+			document.getElementById('show-todo-list-button').innerHTML = "Hide Todo List"
+			// document.getElementById('show-hide-button').innerHTML = '';
 
-		//write the count because the variable wouldn't update after adding it to the top of the previous template
+			iconIndex++;
+
+			//write the count because the variable wouldn't update after adding it to the top of the previous template
+			// document.getElementById('todo-count').innerHTML = `<p>Things to do: ${todoCount}</p>`
+		}
+		else {
+
+			document.getElementById('todo').innerHTML = '';
+			document.getElementById('show-hide-button').innerHTML = '';
+			document.getElementById('show-todo-list-button').innerHTML = "Show Todo List"
+		}
 		document.getElementById('todo-count').innerHTML = `<p>Things to do: ${todoCount}</p>`
 	}
 
@@ -122,6 +152,16 @@ function TodoController() {
 		}
 		else if (showComplete == false) {
 			showComplete = true;
+		}
+		getTodos();
+	}
+
+	this.toggleTodoPane = function () {
+		if (showTodos == true) {
+			showTodos = false;
+		}
+		else if (showTodos == false) {
+			showTodos = true;
 		}
 		getTodos();
 	}
