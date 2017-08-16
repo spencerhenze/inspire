bef4ef180b0b5345b19c584612fdd7b1ff8e525b
Spencer Henze <spencerhenze@Spencers-iMac.local>
2017-08-15 19:49:29 -0600

    Added military time toggle on click of time readout

--- public/app/components/clock/clock.js
+++ public/app/components/clock/clock.js
@@ -1,10 +1,45 @@
 
-var interval = setInterval(function () {
-    myClock();
-}, 1000);
+var militaryTime = false;
 
-myClock = function () {
+function militaryClock() {
     var d = new Date();
-    document.getElementById('clock').innerHTML = d.toLocaleTimeString();
+    document.getElementById('clock').innerHTML = d.toLocaleTimeString('en-US', { hour12: false });
 }
+
+function twelveHrClock() {
+    var d = new Date();
+    document.getElementById('clock').innerHTML = d.toLocaleTimeString('en-US', { hour12: true });
+}
+
+
+function drawClock() {
+    var clock = '';
+    var interval = setInterval(function () {
+        if (militaryTime == true) {
+            militaryClock();
+        }
+        else {
+            twelveHrClock();
+        }
+    }, 1000);
+
+
+}
+
+
+function toggleMilitaryTime() {
+    
+    if (militaryTime == false) {
+        militaryTime = true;
+    }
+    else if (militaryTime == true) {
+        militaryTime = false;
+    }
+    drawClock();
+}
+
+drawClock();
+
+
+
 
